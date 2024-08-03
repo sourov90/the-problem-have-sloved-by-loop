64de029960cd4419bf9d1b2e6f61e5791f7d6ae8
sourov90 <sourovchandra921@gmail.com>
2024-08-03 19:56:57 +0600

    something misetake

---
+++ oddNumber.js
@@ -0,0 +1,14 @@
+// sum of even number by 1 to 100 
+let sum = 0;
+for (let i = 0; i < 101; i += 2) {
+    // console.log(i)
+    sum += i
+}
+console.log('some of odd number 1 to 100 : ', sum)
+
+//sum of odd number 91 to 129
+let sum2 = 0;
+for (let i = 91; i < 130; i++) {
+    sum2 += i
+}
+console.log("The result is , sum of 91 to 129 := ", sum2)
