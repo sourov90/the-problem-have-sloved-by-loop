f8c951f68efc9e906413f9d793ba7b0bde1b972c
sourov90 <sourovchandra921@gmail.com>
2024-08-03 19:58:18 +0600

    added a some problems

--- oddNumber.js
+++ oddNumber.js
@@ -1,6 +1,6 @@
-// sum of even number by 1 to 100 
+// sum of odd number by 1 to 100 
 let sum = 0;
-for (let i = 0; i < 101; i += 2) {
+for (let i = 1; i < 101; i += 2) {
     // console.log(i)
     sum += i
 }
@@ -8,7 +8,7 @@ console.log('some of odd number 1 to 100 : ', sum)
 
 //sum of odd number 91 to 129
 let sum2 = 0;
-for (let i = 91; i < 130; i++) {
+for (let i = 90; i < 130; i+=2) {
     sum2 += i
 }
 console.log("The result is , sum of 91 to 129 := ", sum2)
